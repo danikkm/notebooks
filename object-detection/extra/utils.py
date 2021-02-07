199c8cbdff35ce86cbdc51bf09d5410c806f124b
danikkm <dldanown@gmail.com>
2021-02-08 00:07:48 +0200

    Fix indent, tabs to spaces

--- object-detection/extra/utils.py
+++ object-detection/extra/utils.py
@@ -10,14 +10,14 @@ def fetch(url):
   import requests, os, hashlib, tempfile
   fp = os.path.join(tempfile.gettempdir(), hashlib.md5(url.encode('utf-8')).hexdigest())
   if os.path.isfile(fp) and os.stat(fp).st_size > 0:
-    with open(fp, "rb") as f:
-      dat = f.read()
+  with open(fp, "rb") as f:
+    dat = f.read()
   else:
-    print("fetching %s" % url)
-    dat = requests.get(url).content
-    with open(fp + ".tmp", "wb") as f:
-      f.write(dat)
-    os.rename(fp + ".tmp", fp)
+  print("fetching %s" % url)
+  dat = requests.get(url).content
+  with open(fp + ".tmp", "wb") as f:
+    f.write(dat)
+  os.rename(fp + ".tmp", fp)
   return dat
 
 def load_cifar():
@@ -28,8 +28,8 @@ def load_cifar():
   return X, Y
 
 def imshow(img):
-	img = img / 2 + 0.5
-	npimg = img.numpy()
-	plt.imshow(np.transpose(npimg, (1, 2, 0)))
-	plt.show()
+  img = img / 2 + 0.5
+  npimg = img.numpy()
+  plt.imshow(np.transpose(npimg, (1, 2, 0)))
+  plt.show()
 
