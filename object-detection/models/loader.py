2ba724555d7c8980c27274f58ed68197689d1bd8
danikkm <dldanown@gmail.com>
2021-03-21 16:41:50 +0200

    Replace MobileNet models with Torch models

--- object-detection/models/loader.py
+++ object-detection/models/loader.py
@@ -1,5 +1,4 @@
-from models.mobilenetv2 import MobileNetV2
-from models.mobilenetv3 import MobileNetV3
+import torchvision.models as models
 
 class ModelLoader:
   def __init__(self, batch_size=64, epochs=32):
@@ -7,18 +6,20 @@ class ModelLoader:
     self.batch_size = batch_size
     self.epochs = epochs
   
-  def load_mobilenetv2(self):
-    self.model = MobileNetV2()
+  def load_mobilenetv2(self, pretrained=False, num_classes=10):
+    self.model = models.mobilenet_v2(pretrained=False, num_classes=num_classes)
     self.name = 'mobilenetv2'
     self.checkpoint_path = f'./checkpoint/ckpt_{self.name}_bs{self.batch_size}_ep{self.epochs}.pth'
     
     return self.model, self.name, self.checkpoint_path
   
-  def load_mobilenetv3(self, mode='large', classes_num=10, input_size=32, dropout=0.8, width_multiplier=1.0):
-    self.model = MobileNetV3(mode=mode, classes_num=classes_num,
-                 input_size=input_size, dropout=dropout,
-                 width_multiplier=width_multiplier)
-    self.name = 'mobilenetv3'
+  def load_mobilenetv3(self, mode='large', pretrained=False, num_classes=10):
+    if mode == 'large':
+      self.model = models.mobilenet_v3_large(pretrained=False, num_classes=num_classes)
+    else:
+      self.model = models.mobilenet_v3_small(pretrained=False, num_classes=num_classes)
+
+    self.name = f'mobilenetv3{mode}'
     self.checkpoint_path = f'./checkpoint/ckpt_{self.name}_bs{self.batch_size}_ep{self.epochs}.pth'
     
     return self.model, self.name, self.checkpoint_path  
